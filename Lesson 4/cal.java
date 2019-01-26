60730b8fcbe1d2c2ef9775a6f4ef0b4bb2d9b035
Oleg Cusursuz <olegkusursuz.ok@gmail.com>
2019-01-26 17:35:48 +0200

    AddCalculator

---
+++ Lesson 4/cal.java
@@ -0,0 +1,16 @@
+public class cal {
+	
+	public static void main (String[] args){
+		boolean isEmployed = false;
+		boolean isprogrammer = true;
+		
+		byte age = 25;
+		short salary = 15000;
+		int height = 185;
+		long phoneNumber = 1234567L;//123456798l
+		
+		float massIndex = 25.1F;// 25.1f
+		double massIndexDouble = 28.9;
+		
+	}
+}
---
+++ Lesson 5/AddMinus.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class AddMinus{
+	public static void main (String[] args){
+	Scanner Bender = new Scanner(System.in);
+	
+    double x, y, result;
+	
+	System.out.println("number 1: ");
+	x = Bender.nextDouble();
+	
+	System.out.println("number 2: ");
+	y = Bender.nextDouble();
+	
+	result = x - y;
+	
+	System.out.println(" result " + result);
+	System.out.println(" AddCalculator");
+		System.out.println(" Oleg Cusursuz");
+			System.out.println(" number1 - number2");
+	}
+}
