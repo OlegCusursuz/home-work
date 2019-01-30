68918211ea679c397854df62633d98cdc67c0884
Oleg Cusursuz <olegkusursuz.ok@gmail.com>
2019-01-30 10:01:31 +0200

    WhatsUp

--- Lesson 4/HelloWorld.java
+++ Lesson 4/HelloWorld.java
@@ -1,7 +1,11 @@
 public class HelloWorld{
 public static void main (String[] args){
 System.out.println("\nHello");
+/*
+This is a 
+multiline comment
+*/
 System.out.println("\nJava Fundamentals, Tekwill");
-System.out.println("\noleg");
+System.out.println("\noleg");// This is a singl line
 }
 }
--- Lesson 4/WhatsUp.java
+++ Lesson 4/WhatsUp.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 public class WhatsUp {
 
     public static void main(String[] args){
+		Scanner Bender = new Scanner(System.in);
+		String name, surname, Domen;
+		
         System.out.println("\nHello User");
         System.out.println("\nWhat's your name: ");
+		name = Bender.next();
         System.out.println("\nWhat's your surname: ");
+		surname = Bender.next();
         System.out.println("\nWhat's your Domen: ");
-        Scanner reader = new Scanner(System.in);
-        String Name = reader.next();
-        String Surname = reader.next();
-        String Domen = reader.next();
-        reader.close();
+        Domen = Bender.next();
 
-        System.out.println("Hello!!! " + Name + " " + Surname);
+        System.out.println("Hello!!! " + name + " " + surname);
         System.out.println(Domen);
 
         
